1d55a084a8e5f3994c0761716add15ed8c7b6e73
Rahul Jha <rahulbudy11@gmail.com>
2024-07-22 02:11:25 +0530

    carousal structure completed

--- app/home/carousal.tsx
+++ app/home/carousal.tsx
@@ -1,51 +1,70 @@
-import React from 'react'
+import React, { useState } from 'react'
 const Data = [
     {
         id:1,
-        topic: "test",
+        topic: "test1",
+        img: "imagesrc1"
+
+    },
+    {
+        id:2,
+        topic: "test2",
         img: "imagesrc"
 
     },
     {
-        id:1,
-        topic: "test",
+        id:3,
+        topic: "test3",
         img: "imagesrc"
 
     },
     {
-        id:1,
-        topic: "test",
+        id:4,
+        topic: "test4",
         img: "imagesrc"
 
     },
     {
-        id:1,
-        topic: "test",
+        id:5,
+        topic: "test5",
         img: "imagesrc"
 
     },
     {
-        id:1,
-        topic: "test",
+        id:6,
+        topic: "test6",
         img: "imagesrc"
 
     },
     {
-        id:1,
-        topic: "test",
-        img: "imagesrc"
-
-    },
-    {
-        id:1,
-        topic: "test",
+        id:7,
+        topic: "test7",
         img: "imagesrc"
 
     },
 ]
 function Carousal() {
+  const [carousal, setCarousal] = useState(Data.slice(0,2))
+  const handleNextClick = ( ) => {
+    setCarousal(Data.slice(carousal[1].id,(carousal[1].id+2)))
+    
+  }
+  const handlePrevClick = ( ) => {
+  
+  }
   return (
-    <div>Carousal</div>
+    <div className='flex gap-12 justify-center'>
+        <div onClick={handlePrevClick}>prev</div>
+        <div className='flex gap-12'>
+        {carousal.map( item => 
+
+            <div key={item.id} className='p-2 border border-black w-40 h-20' >{item.topic}</div>
+        )}
+        </div>
+        <div onClick={handleNextClick}>next</div>
+
+    </div>
+    
   )
 }
 
--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,3 +1,4 @@
+'use client'
 import Carousal from "./carousal";
 import NavBar from "./navbar";
 
